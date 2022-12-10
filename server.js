829b6cdfa2d57a7439e63fba497c2a0f47a3f119
Ka Wai Wong <kw.wong0923@gmail.com>
2022-12-09 23:46:59 -0500

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 app.use(express.static(__dirname + "/dist/front-end-survey"));
-app.get("/*", (req, res) =>
-{
-    res.sendFile(path.join(__dirname + "/dist/front-end-survey/index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/front-end-survey/index.html"));
 });
-
 app.listen(process.env.PORT || 8080);
